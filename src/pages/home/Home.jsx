86219f7fa7b3cb35f107645b58a1795fa02ca32b
SabirovaOlya <sabirovaolya44@gmail.com>
2023-12-07 10:17:49 +0500

    last-changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { useQuery } from '@tanstack/react-query';
 import Header from '../../components/Common/Header'
+import axios from 'axios';
 
 function Home() {
+  const { isLoading, error, data } = useQuery({
+    queryKey: 'repoData',
+    queryFn: () =>
+      axios.get(
+        'https://dental-app.financial-project.uz/api/prices'
+      ).then((response) => console.log(response))
+  });
+
+
   return (
     <div>
       <Header title={'Главная'} />
